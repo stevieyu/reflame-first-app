0e864f55844e3547a981cf454bbd1302e795b479
StevieYu <yzm629@sina.cn>
2022-12-30 01:48:22 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      entry: 'src/main.jsx',
+    })
+  ]
 })
